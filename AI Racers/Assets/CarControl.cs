1641e2742d16b5b6520e6f2acb73ff6ff83d0fcb
chris100185 <chris100185@gmail.com>
2018-04-04 22:38:44 -0700

    Update
    Added a player controllable car.

---
+++ AI Racers/Assets/CarControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour {
+
+    public float acceleration;//acceleration rate
+    public float topSpeed;//maximum speed
+    public float turnRate;
+    public float resistance;
+    private Vector2 moveVector; //The vector used to apply movement to the controller.
+    private enum MoveStates { idle, forward,reverse} //States for standing, walking and running.
+    private MoveStates moveStates=MoveStates.idle;
+
+    Rigidbody2D rigid;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        rigid.drag = resistance;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //set state for accelerating and reversing
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            moveStates = MoveStates.forward;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            moveStates = MoveStates.reverse;
+        }
+        else
+        {
+            moveStates = MoveStates.idle;
+        }
+
+        //steering
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Turn(1);
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Turn(-1);
+        }
+
+        //apply movement forces
+        if(moveStates==MoveStates.forward)
+        {
+            Forward();
+        }
+        if(moveStates==MoveStates.reverse)
+        {
+            Reverse();
+        }
+	}
+
+    void Forward()
+    {
+        rigid.AddForce(transform.up * acceleration);
+        
+    }
+    void Reverse()
+    {
+        rigid.AddForce(-transform.up * (acceleration * 0.75f));
+    }
+    void Turn(int direction)//modifier for direction, 1 for left turn, 1 for right turn.
+    {
+        transform.Rotate(Vector3.forward * (direction * turnRate));//((direction * turnRate)(Vector3.forward));
+    }
+}
