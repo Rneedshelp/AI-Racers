406417fddf96694fa845c52174211d3a4297eff2
Mircea Dumitrache <dmircea@csu.fullerton.edu>
2018-04-20 12:50:20 -0700

    new scene added

---
+++ AI Racers/Assets/GraphImplement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphImplement : MonoBehaviour {
+
+	class Node
+	{
+		Transform coordinates;
+		bool visited = false;
+	};
+
+	class GraphAStar
+	{
+		Node root;
+
+	};
+
+
+	[SerializeField]
+	private List<Transform> nodesList;
+
+	// Use this for initialization
+	void Start () {
+		nodesList = new List<Transform> ();
+		foreach (Transform child in transform) {
+			nodesList.Add (child);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
