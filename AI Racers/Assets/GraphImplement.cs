ce3afcbca66c2cc95db2b39c64caffd227b8c00e
chris100185 <chris100185@gmail.com>
2018-04-27 12:26:24 -0700

    Revert "Extra"
    This reverts commit 5935fe74b463c9bb01be7f9237d74a04e510743f.

--- AI Racers/Assets/GraphImplement.cs
+++ AI Racers/Assets/GraphImplement.cs
@@ -13,26 +13,19 @@ public class GraphImplement : MonoBehaviour {
 	class GraphAStar
 	{
 		Node root;
+
 	};
 
 
-    //private GraphAStar track = new GraphAStar();
-    public float size = 0f;
+	[SerializeField]
+	private List<Transform> nodesList;
 
-    [SerializeField]
-    private List<Transform> nodesList;
-
-    // Use this for initialization
-    void Start () {
+	// Use this for initialization
+	void Start () {
 		nodesList = new List<Transform> ();
 		foreach (Transform child in transform) {
 			nodesList.Add (child);
 		}
-
-        size = nodesList[0].GetComponent<SpriteRenderer>().size[0];
-
-
-
 	}
 	
 	// Update is called once per frame
