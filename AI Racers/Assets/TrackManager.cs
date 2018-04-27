99b040d9651d880c6cbab9a916412b540e1ae591
chris100185 <chris100185@gmail.com>
2018-04-27 12:26:12 -0700

    Revert "Merge branch 'master' of https://github.com/chris100185/AI-Racers"
    This reverts commit bc35a6c21c9588194481354d25067c1d191175fa, reversing
    changes made to 5935fe74b463c9bb01be7f9237d74a04e510743f.

--- AI Racers/Assets/TrackManager.cs
+++ AI Racers/Assets/TrackManager.cs
@@ -4,33 +4,13 @@ using UnityEngine;
 
 public class TrackManager : MonoBehaviour {
     public List<GameObject> NodeList;
-    public int totalLaps;
-    int curLap=1;
-    float raceTimer=0;
-    bool raceStart = false;
-
-
-    public GameObject ui;
-    raceUIscript raceUI;
-    // Use this for initialization
-    void Start ()
-    {
-        raceUI = ui.GetComponent<raceUIscript>();
-    }
+	// Use this for initialization
+	void Start () {
+		
+	}
 	
 	// Update is called once per frame
-	void Update ()
-    {
-		//increment the race timer. used for overall calculations as well as for other cars to calculate their times
-        if(raceStart)
-        {
-            raceTimer += Time.deltaTime;
-        }
+	void Update () {
+		
 	}
-
-    public void NextLap()
-    {
-        curLap += 1;
-        raceUI.updateLapCounter(curLap, totalLaps);
-    }
 }
