9c34f8ece3f068152d1220b47822f8e91f0316c7
chris100185 <chris100185@gmail.com>
2018-04-27 10:35:40 -0700

    Added TrackMaanger to hold nodes and handle race elements
    update

---
+++ AI Racers/Assets/TrackManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackManager : MonoBehaviour {
+    public List<GameObject> NodeList;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
